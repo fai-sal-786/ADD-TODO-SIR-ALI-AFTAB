ec6042a2a94277a595c4407d5a4b20575dd9cf53
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-10-27 10:45:18 -0700

    SIR ALI AFTAB

---
+++ java todolist/app1.js
@@ -0,0 +1,33 @@
+var idCount = 1
+
+function addTodo() {
+    var todos = document.getElementById('todos')
+    var inpVal = document.getElementById('inp')
+    var element = document.createElement('div')
+    element.setAttribute('id', idCount)
+    element.setAttribute('class', "todo")
+
+    element.innerHTML = `
+    <p>${inpVal.value}</p> <button onclick="deleteTodo(${idCount})">Delete</button> <button onclick="updateTodo(${idCount})">Update</button>
+   `
+
+    todos.appendChild(element)
+
+    idCount = idCount + 1;
+}
+
+function deleteTodo(id){
+    // console.log("DELETED", id);
+    var deletedItem = document.getElementById(id);
+    var parentElem = deletedItem.parentNode;
+    parentElem.removeChild(deletedItem)
+}
+
+function updateTodo(id){
+    var inpVal = document.getElementById('inp')
+    var todoItem = document.getElementById(id)
+
+    todoItem.innerHTML = `
+    <p>${inpVal.value}</p> <button onclick="deleteTodo(${id})">Delete</button> <button onclick="updateTodo(${id})">Update</button>
+   `
+}
